9f65e7fc91e1baa2a8e50f5362bd35e79e8ae2a6
Erdemozer01 <ozer246@gmail.com>
2025-06-02 05:47:19 +0300

    buzzer

--- dashboard_app/dash_apps.py
+++ dashboard_app/dash_apps.py
@@ -552,8 +552,23 @@ def update_all_graphs(n_intervals):
     estimation_text = "Veri bekleniyor..."
     if not conn or not id_to_plot:
         if conn: conn.close()
-        fig_map.update_layout(title_text='Ortamın 2D Haritası (2D Map of the Environment)',
-                              xaxis_title="X Mesafesi (cm)", yaxis_title="Y Mesafesi (cm)")
+        fig_map.update_layout(
+            title_text='Ortamın 2D Haritası (2D Map of the Environment)',
+            xaxis_title="X Mesafesi (cm)", yaxis_title="Y Mesafesi (cm)",
+            yaxis_scaleanchor="x", yaxis_scaleratio=1,
+            uirevision=id_to_plot,
+            legend=dict(
+                title='Gösterim Katmanları',
+                orientation="h",  # Lejantı yatay olarak sırala
+                yanchor="bottom",  # Dikey konumu alt kenara göre ayarla
+                y=1.02,  # Grafiğin hemen üstüne yerleştir
+                xanchor="right",  # Yatay konumu sağ kenara göre ayarla
+                x=1,  # En sağa hizala
+                bgcolor="rgba(255, 255, 255, 0.7)",  # Yarı saydam arka plan
+                bordercolor="Black",
+                borderwidth=1
+            )
+        )
         return fig_map, fig_polar, fig_time, "Tarama başlatın."
     try:
         df_points = pd.read_sql_query(
