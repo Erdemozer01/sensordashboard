a9b26985c957bb2f1752a01c50ffc0fb70cb6f84
Erdemozer01 <ozer246@gmail.com>
2025-06-06 12:35:04 +0300

    new app

--- dashboard_app/dash_apps.py
+++ dashboard_app/dash_apps.py
@@ -1010,6 +1010,7 @@ def yorumla_model_secimi(selected_model_value):
         commentary_component = dbc.Alert(
             dcc.Markdown(yorum_text_from_ai, dangerously_allow_html=True, link_target="_blank"), color="info")
         image_component = image_generate(yorum_text_from_ai, selected_model_value)
+        print(image_component)
         return [commentary_component, image_component]
 
     points_qs = scan.points.all().values('derece', 'mesafe_cm')
